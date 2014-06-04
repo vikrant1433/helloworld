9b251e1fc7404134902f5f138e02852f410cae53
Vikrant <mailtovikrantpro@gmail.com>
2014-06-05 00:41:28 +0530

    uploading all files

---
+++ app/models/User.java
@@ -0,0 +1,15 @@
+package models;
+
+
+/**
+ * Created by Vikrant on 6/3/2014.
+ */
+public class User {
+    public String name;
+    public Integer age;
+
+    public User(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+}
